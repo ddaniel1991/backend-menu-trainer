bdc48259d33fe72e0ebcf7c6b2c43f831c1fb5f6
ddaniel1991 <david.augustin10@gmail.com>
2022-03-16 09:21:00 -0400

    First model/ First REST/CRUD API

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT;
+
+connectDB()
+
 const app = express();
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use("/api/dishes", require('./routes/dishRoutes'))
 
 
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 
 
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`))
--- controllers/dishController.js
+++ controllers/dishController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('express-async-handler');
 
+const Dish = require('../models/dishModel')
 // @desc Get Dishes
 //@route  GET /api/dishes
 //@access Private
 
 const getDishes = async (req,res) => {
-    res.json({message: "Get Dishes"});
+    const dishes = await Dish.find()
+    res.status(200).json(dishes)
 
 }
 
@@ -14,20 +16,39 @@ const getDishes = async (req,res) => {
 //@access Private
 
 const createDish = asyncHandler(async (req,res) => {
-    if (!req.body.text) {
+    if (!req.body.name) {
         res.status(400)
-        throw new Error('Please add a text field')
+        throw new Error('Please add a dish name')
     }
-    res.json({message: "Create Dish"});
 
-})
+    const dish = await Dish.create({
+
+        name: req.body.name,
+        description: req.body.description
+        
+    })
+
+    
+    res.status(200).json(dish);
+
+}) 
 
 // @desc Update Dish
 //@route  PUT /api/dishes/:id
 //@access Private
 
 const updateDish = asyncHandler(async (req,res) => {
-    res.json({message: `Update Goal ${req.params.id}`});
+
+    const dish = await Dish.findById(req.params.id)
+
+    if(!dish) {
+        res.status(400)
+        throw new Error("Dish not found")
+    }
+
+    const updatedDish = await Dish.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedDish);
 
 })
 
@@ -36,7 +57,15 @@ const updateDish = asyncHandler(async (req,res) => {
 //@access Private
 
 const deleteDish = asyncHandler(async (req,res) => {
-    res.json({message: `Delete Goal ${req.params.id}`});
+    const dish = await Dish.findById(req.params.id)
+
+    if(!dish) {
+        res.status(400)
+        throw new Error("Dish not found")
+    }
+
+    await dish.remove()
+    res.status(200).json({id: req.params.id});
 
 })
 
